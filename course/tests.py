7dc79198a95d23adb2a69c130c321ce2bb4f4b6a
Sherika91 <shermadinil.luka@gmail.com>
2023-11-11 21:28:01 +0300

    Testing Changes

--- course/tests.py
+++ course/tests.py
@@ -1,65 +1,82 @@
-from django.urls import reverse
-from rest_framework import status
-from rest_framework.authtoken.models import Token
-from rest_framework.test import APITestCase, APIClient, APIRequestFactory
-from rest_framework_simplejwt.settings import api_settings
 
-from course.models import Course, Lesson
+from django.db import connection
+from rest_framework import status
+
+from rest_framework.test import APITestCase, APIClient
+from rest_framework_simplejwt.tokens import AccessToken
 from users.models import User
 
 
-class TestCourse(APITestCase):
+class CourseTestCases(APITestCase):
+    email = 'test@gmail.com'
+    password = 'test1234'
 
-    def SetUp(self) -> None:
-        self.client = APIClient()
-        self.user = User.objects.create(email='test@gmail.com', set_password='testpassword')
-
-        self.course = Course.objects.create(
-            name='Test Course',
-            description='Test Description',
-            course_owner=self.user,
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create(
+            email=cls.email,
+            first_name='Admin',
+            last_name='Admin',
+            phone='12345678',
+            role='member',  # Can Be Member or Moderator
+            is_staff=True,
+            is_superuser=True,
+            is_active=True,
         )
+        cls.user.set_password(cls.password)
+        cls.user.save()
+
+    def setUp(self):
+        self.client = APIClient()
+        # Get Access Token For Current User (Moderator)
+        access_token = AccessToken.for_user(self.user)
+        self.client.force_authenticate(user=self.user, token=access_token)
+
+    def test_course_create(self):
+        """ Test For Creating Course Object """
+        print(connection.queries)
+
+        response = self.client.post(path='/course/', data={
+            'name': 'Test Course',
+            'description': 'Test Description',
+            'course_owner_id': self.user.id,
+        })
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_get_course_list(self):
         """ Test To Get Course Objects List """
-        response = self.client.get(
-            path='/course/',
+        response = self.client.get(path='/course/')
 
-        )
-        print(response.json())
-        self.assertEqual(
-            response.status_code,
-            status.HTTP_200_OK,
-        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK, )
 
+        self.assertEqual(response.json(),
+                         {
+                             'count': 1,
+                             'next': None,
+                             'previous': None,
+                             'results': [
+                                 {'id': 1,
+                                  'name': 'Test Course',
+                                  'lesson_list': [],
+                                  'lesson_count': 0,
+                                  'description': 'Test Description',
+                                  'course_owner': 'test@gmail.com'}
+                             ]
+                         }
+                         )
 
-class TestLessons(APITestCase):
-    def setUp(self) -> None:
-        self.client = APIClient()
-        self.user = User.objects.create(email='test@gmail.com', password='testpassword')
-        self.client.force_authenticate(user=self.user)
+    def test_get_course(self):
+        """ Test For Getting Course Object """
+        response = self.client.get('/course/1/')
 
-        self.course = Course.objects.create(
-            name='Test Course',
-            description='Test Description',
-            course_owner=self.user,
-        )
-
-        self.lesson = Lesson.objects.create(
-            name='Test Lesson',
-            description='Test Description',
-            video='https://www.youtube.com'
-
-        )
-
-    def test_get_lessons_list(self):
-        response = self.client.get(
-            reverse='/lesson/'
-
-        )
-
-        self.assertEqual(
-            response.status_code,
-            status.HTTP_200_OK
-        )
-
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json(),
+                         {'id': 1,
+                          'name': 'Test Course',
+                          'lesson_list': [],
+                          'lesson_count': 0,
+                          'description': 'Test Description',
+                          'course_owner': 'test@gmail.com'
+                          }
+                         )
--- users/tests.py
+++ users/tests.py
@@ -1,18 +1,21 @@
+from time import sleep
+
+from django.db.models import Model
 from rest_framework import status
 from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
 
 from .models import User
-from rest_framework.test import APITestCase, APIClient
+from rest_framework.test import APITestCase, APIClient, force_authenticate
 
 
-class UserProfileListTestCase(APITestCase):
+class UserProfileTestCases(APITestCase):
     email = 'user@gmail.com'
     password = 'password'
 
-    def setUp(self) -> None:
-        self.client = APIClient()
-        self.user = User.objects.create(
-            email=self.email,
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create(
+            email=cls.email,
             first_name='Admin',
             last_name='Admin',
             phone='12345678',
@@ -21,188 +24,71 @@ class UserProfileListTestCase(APITestCase):
             is_superuser=True,
             is_active=True,
         )
-        self.user.set_password(self.password)
-        self.user.save()
+        cls.user.set_password(cls.password)
+        cls.user.save()
 
+    def setUp(self):
+        self.client = APIClient()
         # Get Access Token For Current User (Moderator)
         access_token = AccessToken.for_user(self.user)
-        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
+        self.client.force_authenticate(user=self.user, token=access_token)
 
     def test_user_get_profile_list(self):
         response = self.client.get('/users/profile/')
 
-        self.assertEqual(
-            response.status_code,
-            status.HTTP_200_OK
-        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        self.assertEqual(
-            response.json()[0],
-
-            {
-                'id': 1,
-                'payment_history': [],
-                'email': 'user@gmail.com',
-                'first_name': 'Admin',
-                'last_name': 'Admin',
-                'phone': '12345678',
-                'avatar': None,
-                'role': 'moderator'
-            }
-
-        )
-
-
-class UserProfileRetrieveTestCase(APITestCase):
-    email = 'user@gmail.com'
-    password = 'password'
-
-    def setUp(self) -> None:
-        self.client = APIClient()
-        self.user = User.objects.create(
-            email=self.email,
-            first_name='Admin',
-            last_name='Admin',
-            phone='12345678',
-            role='moderator',  # Can Be Member or Moderator
-            is_staff=True,
-            is_superuser=True,
-            is_active=True,
-        )
-        self.user.set_password(self.password)
-        self.user.save()
-
-        # Get Access Token For Current User (Moderator)
-        access_token = AccessToken.for_user(self.user)
-        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
+        self.assertEqual(response.json()[0],
+                         {
+                             'id': 1,
+                             'payment_history': [],
+                             'email': 'user@gmail.com',
+                             'first_name': 'Admin',
+                             'last_name': 'Admin',
+                             'phone': '12345678',
+                             'avatar': None,
+                             'role': 'moderator'
+                         }
+                         )
 
     def test_user_retrieve_profile(self):
         response = self.client.get('/users/profile/1/')
 
-        self.assertEqual(
-            response.status_code,
-            status.HTTP_200_OK
-        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        self.assertEqual(
-            response.json(),
-
-            {
-                'id': 1,
-                'payment_history': [],
-                'email': 'user@gmail.com',
-                'first_name': 'Admin',
-                'last_name': 'Admin',
-                'phone': '12345678',
-                'avatar': None,
-                'role': 'moderator'
-            }
-
-        )
-
-
-class UserProfileUpdateTestCase(APITestCase):
-    email = 'user@gmail.com'
-    password = 'password'
-
-    def setUp(self) -> None:
-        self.client = APIClient()
-        self.user = User.objects.create(
-            email=self.email,
-            first_name='Admin',
-            last_name='Admin',
-            phone='12345678',
-            role='moderator',  # Can Be Member or Moderator
-            is_staff=True,
-            is_superuser=True,
-            is_active=True,
-        )
-        self.user.set_password(self.password)
-        self.user.save()
-
-        # Get Access Token For Current User (Moderator)
-        access_token = AccessToken.for_user(self.user)
-        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
+        self.assertEqual(response.json(),
+                         {
+                             'id': 1,
+                             'payment_history': [],
+                             'email': 'user@gmail.com',
+                             'first_name': 'Admin',
+                             'last_name': 'Admin',
+                             'phone': '12345678',
+                             'avatar': None,
+                             'role': 'moderator'
+                         }
+                         )
 
     def test_user_update_profile(self):
-        response = self.client.put('/users/profile/1/update/',
-                                   data={
-                                       'first_name': 'AdminUpdate',
-                                       'last_name': 'AdminUpdate'
-                                   })
-        print(response.json())
-        self.assertEqual(
-            response.status_code,
-            status.HTTP_200_OK
-        )
+        response = self.client.patch('/users/profile/1/update/',
+                                     data={
+                                         'first_name': 'AdminUpdate',
+                                         'last_name': 'AdminUpdate'
+                                     })
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        self.assertEqual(
-            response.json(),
-            {'id': 1,
-             'payment_history': [],
-             'email': 'user@gmail.com',
-             'first_name': 'AdminUpdate',
-             'last_name': 'AdminUpdate',
-             'phone': '12345678',
-             'avatar': None,
-             'role': 'moderator'}
+        self.assertEqual(response.json(),
+                         {'id': 1,
+                          'payment_history': [],
+                          'email': 'user@gmail.com',
+                          'first_name': 'AdminUpdate',
+                          'last_name': 'AdminUpdate',
+                          'phone': '12345678',
+                          'avatar': None,
+                          'role': 'moderator'
+                          }
+                         )
 
-        )
-
-
-class UserAuthTestCase(APITestCase):
-    email = 'test@gmail.com'
-    password = 'testpassword'
-
-    def setUp(self) -> None:
-        self.client = APIClient()
-        self.user = User.objects.create(
-            email=self.email,
-            first_name='Admin',
-            last_name='Admin',
-            phone='12345678',
-            is_staff=True,
-            is_superuser=True,
-            is_active=True,
-        )
-        self.user.set_password(self.password)
-        self.user.save()
-
-    def test_successful_token_obtain_pair_gives_200(self):
-        access_token = AccessToken.for_user(self.user)
-        access_token = str(access_token)
-
-        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
-
-        response = self.client.post(
-            '/users/api/token/',
-            {
-                'email': self.email,
-                'password': self.password
-            },
-
-        )
-
-        self.assertEqual(
-            response.status_code,
-            status.HTTP_200_OK
-        )
-
-    def test_successful_token_refresh_gives_200(self):
-        access_token = RefreshToken.for_user(self.user)
-        access_token = str(access_token)
-
-        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
-
-        response = self.client.post(
-            '/users/api/token/refresh/',
-            {
-                'refresh': access_token,
-            },
-
-        )
-
-        self.assertEqual(
-            response.status_code,
-            status.HTTP_200_OK
-        )
+    def test_user_delete_profile(self):
+        response = self.client.delete('/users/profile/1/delete/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
